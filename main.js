542c2d151d2526cc957d40d8f17014f285627386
Cuni <cunigramajo@hotmail.com>
2025-12-11 21:55:39 -0300

    Agregando crc y deteccion de tipo conexion

--- main.js
+++ main.js
@@ -40,23 +40,34 @@ app.on('activate', () => {
 
 // ========== HID Integration ==========
 
+const CRC32 = require('crc-32');
 const HID = require('node-hid');
+
 
 class DualSense {
     constructor(device) {
       this.device = device;
     
-      const devices = HID.devices();
+      const allDevices = HID.devices();
 
-      device = devices.find(({ productId, vendorId }) => productId === 3302 && vendorId === 1356);
+      const devices = allDevices.filter(d => d.vendorId === 1356 && (d.productId === 3302 || d.productId === 3570));
+
+      console.log(`Dispositivos HID encontrados:`, devices);
+
+      device = devices.find(d => d.productId === 3302) || devices.find(d => d.productId === 3570);
 
       if(device === undefined) {
         throw new Error('Controlador no encontrado.');
       }
 
       this.controller = new HID.HID(device.path);
+
+      this.deviceConnection = device.interface;
+      this.isBluetooth = this.deviceConnection === -1 ? true : false;
+
       if (this.controller) {
-        console.log('Controlador DualSense conectado.\n',this.controller.getDeviceInfo());
+        console.log(`Controlador DualSense conectado por ${this.deviceConnection === -1 ? "Bluetooth" : "USB"}\n`,this.controller.getDeviceInfo());
+  
       }
     }
 
@@ -69,10 +80,27 @@ class DualSense {
 	}
 
     report(data, sendData){
-      const paquete = new Array(64).fill(0);
+
+      const length = this.isBluetooth ? 78 : 64; 
+
+      const paquete = new Array(length).fill(0);
       for (const i in data){
         const[index, value] = data[i];
         paquete[index] = value;
+      }
+
+      if(this.isBluetooth){
+        const dataForCrc = paquete.slice(0, 74);
+        
+        const crcChecksum = CRC32.buf(Buffer.from(dataForCrc));
+
+        const unsignedCrc = crcChecksum >>> 0;
+
+        paquete[74] = (unsignedCrc & 0x000000FF);
+        paquete[75] = (unsignedCrc & 0x0000FF00) >> 8;
+        paquete[76] = (unsignedCrc & 0x00FF0000) >> 16;
+        paquete[77] = (unsignedCrc & 0xFF000000) >> 24;
+
       }
 
       if(sendData){
@@ -88,34 +116,40 @@ class DualSense {
 			throw new Error('Colors have values from 0 to 255 only');
 		}
 
-		return this.reportUSB(r, g, b, sendData);
-	}
-
-  	reportUSB(r,g,b,sendData = true) {
-
-		return this.report([
-			[0, 0x2],
-			[2, 0x4],
-			[45, r],
-			[46, g],
-			[47, b],
-		], sendData);
-	}
+		  return this.isBluetooth ? this.reportBT(r,g,b,sendData) : this.reportUSB(r,g,b,sendData);
+	  }
 
 	  reportBT(r,g,b,sendData = true) {
 
 		return this.report([
 			[0, 0x31],
 			[1, 0x02],
-      [3, 0x04],
+      [2, 0xFF],
+      [3, 0x57],
+      [43, 0x01],
+      [44,0x00],
 			[46, r],
 			[47, g],
 			[48, b],
 		], sendData);
 	  }
-}
 
+    reportUSB(r,g,b,sendData = true) {
+
+      return this.report([
+			  [0, 0x2],
+			  [2, 0x4],
+			  [45, r],
+			  [46, g],
+			  [47, b],
+		  ],sendData);
+    }
+}
 
 const controller = new DualSense();
 
-controller.setColor(255, 255, 0, true);
+
+setTimeout(() => {
+  controller.setColor(255, 255, 0, true);
+}, 2000);
+
