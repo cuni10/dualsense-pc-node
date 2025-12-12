0ab22b8f4cfe1ead5247e25f2a5b83925f883e8d
Cuni <cunigramajo@hotmail.com>
2025-12-12 01:33:23 -0300

    Aplicando prettier

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 // ========== Main windows ==========
 
-const {app, BrowserWindow, ipcMain} = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
       nodeIntegration: false,
-      contextIsolation: true
-    }
+      contextIsolation: true,
+    },
   });
 
-  mainWindow.loadFile('index.html');
+  mainWindow.loadFile("index.html");
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  ipcMain.handle('set-led-color', async (event, data) => {
+  ipcMain.handle("set-led-color", async (event, data) => {
     const { r, g, b, sendData } = data;
     return controller.setColor(r, g, b, sendData);
   });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
@@ -40,116 +40,125 @@ app.on('activate', () => {
 
 // ========== HID Integration ==========
 
-const CRC32 = require('crc-32');
-const HID = require('node-hid');
-
+const CRC32 = require("crc-32");
+const HID = require("node-hid");
 
 class DualSense {
-    constructor(device) {
-      this.device = device;
-    
-      const allDevices = HID.devices();
+  constructor(device) {
+    this.device = device;
 
-      const devices = allDevices.filter(d => d.vendorId === 1356 && (d.productId === 3302 || d.productId === 3570));
+    const allDevices = HID.devices();
 
-      console.log(`Dispositivos HID encontrados:`, devices);
+    const devices = allDevices.filter(
+      (d) =>
+        d.vendorId === 1356 && (d.productId === 3302 || d.productId === 3570)
+    );
 
-      device = devices.find(d => d.productId === 3302) || devices.find(d => d.productId === 3570);
+    console.log(`Dispositivos HID encontrados:`, devices);
 
-      if(device === undefined) {
-        throw new Error('Controlador no encontrado.');
-      }
+    device =
+      devices.find((d) => d.productId === 3302) ||
+      devices.find((d) => d.productId === 3570);
 
-      this.controller = new HID.HID(device.path);
-
-      this.deviceConnection = device.interface;
-      this.isBluetooth = this.deviceConnection === -1 ? true : false;
-
-      if (this.controller) {
-        console.log(`Controlador DualSense conectado por ${this.deviceConnection === -1 ? "Bluetooth" : "USB"}\n`,this.controller.getDeviceInfo());
-  
-      }
+    if (device === undefined) {
+      throw new Error("Controlador no encontrado.");
     }
 
-    sendData(data) {
-		if (this.data) {
-			console.log(`[sendData]`, data);
-		}
+    this.controller = new HID.HID(device.path);
 
-		return this.controller.write(data);
-	}
+    this.deviceConnection = device.interface;
+    this.isBluetooth = this.deviceConnection === -1 ? true : false;
 
-    report(data, sendData){
+    if (this.controller) {
+      console.log(
+        `Controlador DualSense conectado por ${
+          this.deviceConnection === -1 ? "Bluetooth" : "USB"
+        }\n`,
+        this.controller.getDeviceInfo()
+      );
+    }
+  }
 
-      const length = this.isBluetooth ? 78 : 64; 
-
-      const paquete = new Array(length).fill(0);
-      for (const i in data){
-        const[index, value] = data[i];
-        paquete[index] = value;
-      }
-
-      if(this.isBluetooth){
-        const dataForCrc = paquete.slice(0, 74);
-        
-        const crcChecksum = CRC32.buf(Buffer.from(dataForCrc));
-
-        const unsignedCrc = crcChecksum >>> 0;
-
-        paquete[74] = (unsignedCrc & 0x000000FF);
-        paquete[75] = (unsignedCrc & 0x0000FF00) >> 8;
-        paquete[76] = (unsignedCrc & 0x00FF0000) >> 16;
-        paquete[77] = (unsignedCrc & 0xFF000000) >> 24;
-
-      }
-
-      if(sendData){
-        this.sendData(paquete);
-      }
-
-      return paquete;
+  sendData(data) {
+    if (this.data) {
+      console.log(`[sendData]`, data);
     }
 
-    setColor(r, g, b, sendData) {
+    return this.controller.write(data);
+  }
 
-		if( (r > 255 || g > 255 || b > 255) || (r < 0 || g < 0 || b < 0) ){
-			throw new Error('Colors have values from 0 to 255 only');
-		}
+  report(data, sendData) {
+    const length = this.isBluetooth ? 78 : 64;
 
-		  return this.isBluetooth ? this.reportBT(r,g,b,sendData) : this.reportUSB(r,g,b,sendData);
-	  }
-
-	  reportBT(r,g,b,sendData = true) {
-
-		return this.report([
-			[0, 0x31],
-			[1, 0x02],
-      [2, 0xFF],
-      [3, 0x57],
-      [43, 0x01],
-      [44,0x00],
-			[46, r],
-			[47, g],
-			[48, b],
-		], sendData);
-	  }
-
-    reportUSB(r,g,b,sendData = true) {
-
-      return this.report([
-			  [0, 0x2],
-			  [2, 0x4],
-			  [45, r],
-			  [46, g],
-			  [47, b],
-		  ],sendData);
+    const paquete = new Array(length).fill(0);
+    for (const i in data) {
+      const [index, value] = data[i];
+      paquete[index] = value;
     }
+
+    if (this.isBluetooth) {
+      const dataForCrc = paquete.slice(0, 74);
+
+      const crcChecksum = CRC32.buf(Buffer.from(dataForCrc));
+
+      const unsignedCrc = crcChecksum >>> 0;
+
+      paquete[74] = unsignedCrc & 0x000000ff;
+      paquete[75] = (unsignedCrc & 0x0000ff00) >> 8;
+      paquete[76] = (unsignedCrc & 0x00ff0000) >> 16;
+      paquete[77] = (unsignedCrc & 0xff000000) >> 24;
+    }
+
+    if (sendData) {
+      this.sendData(paquete);
+    }
+
+    return paquete;
+  }
+
+  setColor(r, g, b, sendData) {
+    if (r > 255 || g > 255 || b > 255 || r < 0 || g < 0 || b < 0) {
+      throw new Error("Colors have values from 0 to 255 only");
+    }
+
+    return this.isBluetooth
+      ? this.reportBT(r, g, b, sendData)
+      : this.reportUSB(r, g, b, sendData);
+  }
+
+  reportBT(r, g, b, sendData = true) {
+    return this.report(
+      [
+        [0, 0x31],
+        [1, 0x02],
+        [2, 0xff],
+        [3, 0x57],
+        [43, 0x01],
+        [44, 0x00],
+        [46, r],
+        [47, g],
+        [48, b],
+      ],
+      sendData
+    );
+  }
+
+  reportUSB(r, g, b, sendData = true) {
+    return this.report(
+      [
+        [0, 0x2],
+        [2, 0x4],
+        [45, r],
+        [46, g],
+        [47, b],
+      ],
+      sendData
+    );
+  }
 }
 
 const controller = new DualSense();
 
-
 setTimeout(() => {
   controller.setColor(255, 255, 0, true);
 }, 2000);
-
--- preload.js
+++ preload.js
@@ -1,6 +1,7 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld('electronAPI', {
-    // Expone una función que envía un mensaje IPC al proceso principal
-    setColor: (r, g, b,sendData) => ipcRenderer.invoke('set-led-color', { r, g, b,sendData })
+contextBridge.exposeInMainWorld("electronAPI", {
+  // Expone una función que envía un mensaje IPC al proceso principal
+  setColor: (r, g, b, sendData) =>
+    ipcRenderer.invoke("set-led-color", { r, g, b, sendData }),
 });
